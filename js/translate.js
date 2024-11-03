c667c96b11a349ae8a4cd5ca803164867934a9b8
esmerquenn <esmereyvazov@gmail.com>
2024-11-03 11:39:43 +0400

    translate 2

--- js/translate.js
+++ js/translate.js
@@ -1,90 +1,3 @@
-// const translations = {
-//   az: {},
-//   en: {},
-//   ru: {},
-// };
-
-// function changeLanguage(language) {
-//   localStorage.setItem("selectedLanguage", language);
-
-//   document.querySelectorAll("[data-key]").forEach((element) => {
-//     const key = element.getAttribute("data-key");
-//     element.innerHTML = translations[language][key];
-//   });
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const selectedLanguage = localStorage.getItem("selectedLanguage") || "AZ";
-
-//   document.querySelectorAll("[data-key]").forEach((element) => {
-//     const key = element.getAttribute("data-key");
-//     element.innerHTML = translations[selectedLanguage][key];
-//   });
-//   const selectedElement = document.querySelector("[x-text='selected === '' ? 'AZ' : selected']");
-//   if (selectedElement) {
-//     selectedElement.textContent = selectedLanguage.toUpperCase();
-//   }
-// });
-
-// const translations = {
-//   az: {
-//     location: "Baku Azərbaycan",
-//     home: "Əsas səhifə",
-//     about: "Haqqımızda",
-//     services: "Xidmətlərimiz",
-//     gallery: "Qalereya",
-//     tours: "Turlar",
-//     contact: "Əlaqə",
-//   },
-//   en: {
-//     location: "Baku, Azerbaijan",
-//     home: "Home",
-//     about: "About Us",
-//     services: "Our Services",
-//     gallery: "Gallery",
-//     tours: "Tours",
-//     contact: "Contact",
-//   },
-//   ru: {
-//     location: "Баку, Азербайджан",
-//     home: "Главная",
-//     about: "О нас",
-//     services: "Наши услуги",
-//     gallery: "Галерея",
-//     tours: "Туры",
-//     contact: "Контакт",
-//   },
-// };
-
-// function changeLanguage(language) {
-//   localStorage.setItem("selectedLanguage", language);
-
-//   document.querySelectorAll("[data-key]").forEach((element) => {
-//     const key = element.getAttribute("data-key");
-//     element.innerHTML = translations[language][key];
-//   });
-
-//   // Dil seçimini göstermek için öğeyi güncelle
-//   const selectedElement = document.querySelector("[x-text='selected === '' ? 'AZ' : selected']");
-//   if (selectedElement) {
-//     selectedElement.textContent = language.toUpperCase();
-//   }
-// }
-
-// document.addEventListener("DOMContentLoaded", function () {
-//   const selectedLanguage = localStorage.getItem("selectedLanguage") || "az";
-
-//   document.querySelectorAll("[data-key]").forEach((element) => {
-//     const key = element.getAttribute("data-key");
-//     element.innerHTML = translations[selectedLanguage][key];
-//   });
-
-//   // Dil seçimini göstermek için öğeyi güncelle
-//   const selectedElement = document.querySelector("[x-text='selected === '' ? 'AZ' : selected']");
-//   if (selectedElement) {
-//     selectedElement.textContent = selectedLanguage.toUpperCase();
-//   }
-// });
 const translations = {
   az: {
     home: "Əsas səhifə",
@@ -99,6 +12,43 @@ const translations = {
     currency_azn: "AZN",
     currency_usd: "USD",
     currency_eur: "EUR",
+    slide1_title: "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın!",
+    slide1_description:
+      "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın! LocalTransfer.com ilə şəhərdaxili və hava limanı transferlərinizi asanlıqla planlaşdırın. Təcrübəli sürücülər və rahat avtomobillər ilə səyahətiniz hər zaman təhlükəsiz və komfortlu olacaq. Birbaşa sifariş",
+    slide2_title: "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın!",
+    slide2_description:
+      "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın! LocalTransfer.com ilə şəhərdaxili və hava limanı transferlərinizi asanlıqla planlaşdırın. Təcrübəli sürücülər və rahat avtomobillər ilə səyahətiniz hər zaman təhlükəsiz və komfortlu olacaq. Birbaşa sifariş",
+    slide3_title: "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın!",
+    slide3_description:
+      "Sizin Yerli Transfer Xidmətiniz — Rahat və Güvənli Səyahətə Başlayın! LocalTransfer.com ilə şəhərdaxili və hava limanı transferlərinizi asanlıqla planlaşdırın. Təcrübəli sürücülər və rahat avtomobillər ilə səyahətiniz hər zaman təhlükəsiz və komfortlu olacaq. Birbaşa sifariş",
+    step1_label: "Sürüş detallarını daxil edin",
+    step2_label: "Avtomobil seçin",
+    step3_label: "Əlaqə məlumatlarını daxil edin",
+    step4_label: "Sifariş detallarına bax",
+    ride_details_title: "SÜRÜŞ DETALLARI",
+    pickup_date_label: "Götürmə tarixi",
+    pickup_time_label: "Götürmə vaxtı",
+    route_label: "Marşrut",
+    select_route: "Marşrutu seçin",
+    route_naxcivan: "Naxçıvan",
+    route_lenkeran: "Lənkəran",
+    route_sirvan: "Şirvan",
+    route_qebele: "Qəbələ",
+    route_agdam: "Ağdam",
+    route_masalli: "Masallı",
+    transfer_type_label: "Transfer tipi",
+    one_way_option: "Bir istiqamət",
+    total_distance_label: "Ümumi məsafə",
+    total_time_label: "Ümumi vaxt",
+    total_label: "Ümumi",
+    vehicle_filter_title: "AVTOMOBİL FİLTERİ",
+    passenger_label: "Sərnişinlər",
+    bag_label: "Çamadanlar",
+    standard_label: "Standart",
+    type_label: "Standart",
+    select_option: "Seçin",
+    all_vehicles: "Bütün avtomobillər",
+    minivan: "Mini van",
   },
   en: {
     home: "Home",
@@ -113,6 +63,44 @@ const translations = {
     currency_azn: "AZN",
     currency_usd: "USD",
     currency_eur: "EUR",
+    slide1_title: "Your Local Transfer Service - Start a Comfortable and Safe Journey!",
+    slide1_description:
+      "Your Local Transfer Service - Start a Comfortable and Safe Journey! Plan your city and airport transfers easily with LocalTransfer.com. With experienced drivers and comfortable vehicles, your journey will always be safe and comfortable. Book directly",
+    slide2_title: "Your Local Transfer Service - Start a Comfortable and Safe Journey!",
+    slide2_description:
+      "Your Local Transfer Service - Start a Comfortable and Safe Journey! Plan your city and airport transfers easily with LocalTransfer.com. With experienced drivers and comfortable vehicles, your journey will always be safe and comfortable. Book directly",
+    slide3_title: "Your Local Transfer Service - Start a Comfortable and Safe Journey!",
+    slide3_description:
+      "Your Local Transfer Service - Start a Comfortable and Safe Journey! Plan your city and airport transfers easily with LocalTransfer.com. With experienced drivers and comfortable vehicles, your journey will always be safe and comfortable. Book directly",
+    step1_label: "Enter ride details",
+    step2_label: "Select vehicle",
+    step3_label: "Enter contact information",
+    step4_label: "Review order details",
+
+    ride_details_title: "RIDE DETAILS",
+    pickup_date_label: "Pickup Date",
+    pickup_time_label: "Pickup Time",
+    route_label: "Route",
+    select_route: "Select a route",
+    route_naxcivan: "Nakhchivan",
+    route_lenkeran: "Lankaran",
+    route_sirvan: "Shirvan",
+    route_qebele: "Gabala",
+    route_agdam: "Agdam",
+    route_masalli: "Masalli",
+    transfer_type_label: "Transfer Type",
+    one_way_option: "One Way",
+    total_distance_label: "Total Distance",
+    total_time_label: "Total Time",
+    total_label: "Total",
+    vehicle_filter_title: "VEHICLE FILTER",
+    passenger_label: "Passengers",
+    bag_label: "Bags",
+    standard_label: "Standard",
+    type_label: "Type",
+    select_option: "Select",
+    all_vehicles: "All vehicles",
+    minivan: "Mini van",
   },
   ru: {
     home: "Главная",
@@ -127,6 +115,44 @@ const translations = {
     currency_azn: "AZН",
     currency_usd: "USD",
     currency_eur: "ЕВР",
+    slide1_title: "Ваш местный трансфер - начните комфортное и безопасное путешествие!",
+    slide1_description:
+      "Ваш местный трансфер - начните комфортное и безопасное путешествие! Легко планируйте городские и аэропортовые трансферы с LocalTransfer.com. Опытные водители и комфортабельные автомобили обеспечат вашу безопасность и комфорт. Заказывайте напрямую",
+    slide2_title: "Ваш местный трансфер - начните комфортное и безопасное путешествие!",
+    slide2_description:
+      "Ваш местный трансфер - начните комфортное и безопасное путешествие! Легко планируйте городские и аэропортовые трансферы с LocalTransfer.com. Опытные водители и комфортабельные автомобили обеспечат вашу безопасность и комфорт. Заказывайте напрямую",
+    slide3_title: "Ваш местный трансфер - начните комфортное и безопасное путешествие!",
+    slide3_description:
+      "Ваш местный трансфер - начните комфортное и безопасное путешествие! Легко планируйте городские и аэропортовые трансферы с LocalTransfer.com. Опытные водители и комфортабельные автомобили обеспечат вашу безопасность и комфорт. Заказывайте напрямую",
+    step1_label: "Введите детали поездки",
+    step2_label: "Выберите автомобиль",
+    step3_label: "Введите контактную информацию",
+    step4_label: "Просмотрите детали заказа",
+
+    ride_details_title: "ДЕТАЛИ ПОЕЗДКИ",
+    pickup_date_label: "Дата забора",
+    pickup_time_label: "Время забора",
+    route_label: "Маршрут",
+    select_route: "Выберите маршрут",
+    route_naxcivan: "Нахичевань",
+    route_lenkeran: "Ленкорань",
+    route_sirvan: "Ширван",
+    route_qebele: "Габала",
+    route_agdam: "Агдам",
+    route_masalli: "Масаллы",
+    transfer_type_label: "Тип трансфера",
+    one_way_option: "В одну сторону",
+    total_distance_label: "Общее расстояние",
+    total_time_label: "Общее время",
+    total_label: "Итог",
+    vehicle_filter_title: "ФИЛЬТР АВТОМОБИЛЕЙ",
+    passenger_label: "Пассажиры",
+    bag_label: "Чемоданы",
+    standard_label: "Стандарт",
+    type_label: "Тип",
+    select_option: "Выбрать",
+    all_vehicles: "Все автомобили",
+    minivan: "Минивэн",
   },
 };
 
